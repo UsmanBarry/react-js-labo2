6749d5cd19c6277da68cc50d91f739ecaaecf400
UsmanBarry <ousmanekakashi98@gmail.com>
2022-10-20 13:12:31 -0400

    formatif_Etape8

--- src/components/ajout2.jsx
+++ src/components/ajout2.jsx
@@ -1,9 +1,51 @@
 import React from "react";
 class Navbar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            local: 'T306 ',
+            batisse: '3030 ',
+
+
+        }
+
+        this.onChangeLocal = this.onChangeLocal.bind(this);
+        this.onChangeBatisse = this.onChangeBatisse.bind(this);
+
+
+    }
+    onChangeLocal(e) {
+        this.setState({
+            Local: e.target.value
+        })
+    }
+    onChangeBatisse(e) {
+        this.setState({
+            Batisse: e.target.value
+        })
+    }
+
+
     render() {
         return (
             <div className="container">
-                Liste est chargé
+                <h3>Liste et recuperation</h3>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label>Local:</label>
+                        <input type="text"
+                            required
+                            className="form-control" value={this.state.local} onChange={this.onChange} />
+                        <label>Batisse:</label>
+                        <input type="text"
+                            required
+                            className="form-control" value={this.state.batisse} onChange={this.onChange} />
+
+                    </div>
+                    <div className="form-group">
+                        <input type="submit" value="Ok" className="btn btn-primary" />
+                    </div>
+                </form>
             </div>
         )
     }
