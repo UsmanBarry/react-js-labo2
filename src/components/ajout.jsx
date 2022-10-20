9c518e49ffbb605a2d984292df2e98ececa9f59d
UsmanBarry <ousmanekakashi98@gmail.com>
2022-10-20 12:39:51 -0400

    commit_avnt_formatif

--- src/components/ajout.jsx
+++ src/components/ajout.jsx
@@ -3,12 +3,43 @@ class ajout extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            code: 'test',
-            nom: 'testnom',
-            prenom: 'testprenom'
+            code: ' ',
+            nom: ' ',
+            prenom: ' '
 
         }
+
+        this.onChangeCode = this.onChangeCode.bind(this);
+        this.onChangeNom = this.onChangeNom.bind(this);
+        this.onChangePrenom = this.onChangePrenom.bind(this);
+
+
     }
+    onChangeCode(e) {
+        this.setState({
+            code: e.target.value
+        })
+    }
+    onChangeNom(e) {
+        this.setState({
+            nom: e.target.value
+        })
+    }
+    onChangePrenom(e) {
+        this.setState({
+            prenom: e.target.value
+        })
+    }
+    onSubmit(e) {
+        e.preventDefault();
+        const util = {
+            code: this.state.code,
+            nom: this.state.nom,
+            prenom: this.state.prenom
+        }
+        console.log(util);
+    }
+
     render() {
         return (
             <div className="container">
@@ -18,15 +49,15 @@ class ajout extends React.Component {
                         <label>Nom:</label>
                         <input type="text"
                             required
-                            className="form-control" value={this.state.nom} />
+                            className="form-control" value={this.state.nom} onChange={this.onChange} />
                         <label>Prenom:</label>
                         <input type="text"
                             required
-                            className="form-control" value={this.state.prenom} />
+                            className="form-control" value={this.state.prenom} onChange={this.onChange} />
                         <label>Code:</label>
                         <input type="text"
                             required
-                            className="form-control" value={this.state.code} />
+                            className="form-control" value={this.state.code} onChange={this.onChange} />
                     </div>
                     <div className="form-group">
                         <input type="submit" value="Ajout" className="btn btn-primary" />
